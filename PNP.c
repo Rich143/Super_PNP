12ab6c0f1e4b6a589aedaf3df9b7fab8b816d4ea
octHarris <sepoct6@yahoo.ca>
2014-11-11 18:10:34 -0500

    tested/fixed singular arm movements

--- PNP.c
+++ PNP.c
@@ -1,41 +1,99 @@
+//@Encoder: the current encoder value for the motor
+//@endAngle: the desired end angle
+//@dir: bool true is left/up, false is right/down
+bool reachedAngle(int encoder, int endAngle, int dir)
+{
+	if (dir)
+	{
+		if (encoder > endAngle)
+		{
+			nxtDisplayString(1, "1Cur: %d", encoder);
+			nxtDisplayString(2, "1end: %d", endAngle);
+			nxtDisplayString(3, "1Dir: %d", dir);
+			return true;
+		}
+	}
+	else
+	{
+		if (encoder > endAngle)
+		{
+			nxtDisplayString(1, "2Cur: %d", encoder);
+			nxtDisplayString(2, "2end: %d", endAngle);
+			nxtDisplayString(3, "2Dir: %d", dir);
+			return true;
+		}
+	}
+	return false;
+}
+
+//@armOneUp: positive angles are up
+//@armTwoUp: positive angles are up
 // @angle: Angle to rotate motor A, cCW being positive
 void move(int speed, int armOneUp, int armTwoUp, int rotate)
 {
 	bool moving  = false, done = false;
 	bool motorsOff[3] = {0, 0, 0};
+	bool leftA = false, upB = false, upC = false;
+	int endAngleA, endAngleB, endAngleC;
+
+	motor[motorA]= 0;
+	motor[motorB]= 0;
+	motor[motorC]= 0;
 
 	while (!done)
 	{
 		if (!moving)
 		{
 			if(armOneUp > 0)
-				motor[motorA] = speed;
-			else
-				motor[motorA] = -speed;
+			{
+				motor[motorB] = speed;
+				endAngleB = nMotorEncoder[motorB] + 6*armOneUp;
+				upB = true;
+			}
+			else if(armOneUp < 0)
+			{
+				motor[motorB] = -speed;
+				endAngleB = nMotorEncoder[motorB] + 6*armOneUp;
+			}
 
 			if (armTwoUp > 0)
-				motor[motorA] = speed;
-			else
-				motor[motorB] = -speed;
-
-			if (angle > 0)
+			{
 				motor[motorC] = speed;
-			else
+				endAngleC = nMotorEncoder[motorC] + 3*armTwoUp;
+				upC = true;
+			}
+			else if (armTwoUp < 0)
+			{
 				motor[motorC] = -speed;
+				endAngleC = nMotorEncoder[motorC] - 3*armTwoUp;
+			}
+
+			if (rotate > 0)
+			{
+				motor[motorA] = speed;
+				endAngleA = nMotorEncoder[motorA] + 7*rotate;
+				//nxtDisplayString(0, "Turning");
+				leftA = true;
+			}
+			else if (rotate < 0)
+			{
+				motor[motorA] = -speed;
+				endAngleA = nMotorEncoder[motorA] - 7*rotate;
+			}
 
 			moving = true;
 		}
-		if (abs(nMotorEncoder[motorA]) < abs(angle))
+		if (reachedAngle(nMotorEncoder[motorA], endAngleA, leftA))
 		{
 			motor[motorA] = 0;
 			motorsOff[0] = true;
 		}
-		if (abs(nMotorEncoder[motorB]) < abs(armOneUp))
+		if (reachedAngle(nMotorEncoder[motorB], endAngleB, upB))
 		{
 			motor[motorB] = 0;
 			motorsOff[1] = true;
 		}
-		if (abs(nMotorEncoder[motorC]) < abs(armOneUp))
+		if (reachedAngle(nMotorEncoder[motorC], endAngleC, upC))
 		{
 			motor[motorC] = 0;
 			motorsOff[2] = true;
@@ -47,6 +105,7 @@ void move(int speed, int armOneUp, int armTwoUp, int rotate)
 		}
 	}
 }
+
 
 void calibrateMotor(tMotor motor_name)
 {
@@ -68,57 +127,63 @@ void calibrateMotor(tMotor motor_name)
 	while(nNxtButtonPressed != -1);
 }
 
+// set the motors to the desired start position and resets encoders
 void calibrate()
 {
 		calibrateMotor(motorA);
 		calibrateMotor(motorB);
 		calibrateMotor(motorC);
+		nMotorEncoder[motorA] = 0;
+		nMotorEncoder[motorB] = 0;
+		nMotorEncoder[motorC] = 0;
 }
 
-
-
-//Gear ratio, motorB to arm: 1:5
 void rotateLeft(int speed, int angle)
 {
 	motor[motorA] = speed;
-	nMotorEncoder[motorA] = 0;
-	while (nMotorEncoder[motorA] < angle);
+	int endAngle = nMotorEncoder[motorA] + 7*angle;
+	while (nMotorEncoder[motorA] < endAngle);
 	motor[motorA] = 0;
 }
 
 void rotateRight(int speed, int angle)
 {
 	motor[motorA] = -speed;
-	nMotorEncoder[motorA] = 0;
-	while (nMotorEncoder[motorA] > angle);
+	int endAngle = nMotorEncoder[motorA] - 7*angle;
+	while (nMotorEncoder[motorA] > endAngle);
 	motor[motorA] = 0;
 }
 void armOneUp(int speed, int angle)
 {
 	motor[motorB] = speed;
-	nMotorEncoder[motorB] = 0;
-	while (nMotorEncoder[motorB] < 5*angle);
+	int endAngle = nMotorEncoder[motorB] + 5*angle;
+	while (nMotorEncoder[motorB] < endAngle);
 	motor[motorB] = 0;
 }
 void armOneDown(int speed, int angle)
 {
 	motor[motorB] = -speed;
-	nMotorEncoder[motorB] = 0;
-	while (nMotorEncoder[motorB] > 5*angle);
+	int endAngle = nMotorEncoder[motorB] - 5*angle;
+
+
+	while (nMotorEncoder[motorB] > endAngle)
+	{
+		nxtDisplayString(0, "the angle is %d", nMotorEncoder[motorA]);
+	}
 	motor[motorB] = 0;
 }
 void armTwoUp(int speed, int angle)
 {
 	motor[motorC] = speed;
-	nMotorEncoder[motorC] = 0;
-	while (nMotorEncoder[motorC] < angle);
+	int endAngle = nMotorEncoder[motorC] + 3 * angle;
+	while (nMotorEncoder[motorC] < endAngle);
 	motor[motorC] = 0;
 }
 void armTwoDown(int speed, int angle)
 {
 	motor[motorC] = -speed;
-	nMotorEncoder[motorC] = 0;
-	while (nMotorEncoder[motorC] > angle);
+	int endAngle = nMotorEncoder[motorC] - 3 * angle;
+	while (nMotorEncoder[motorC] > endAngle);
 	motor[motorC] = 0;
 }
 
@@ -126,9 +191,10 @@ void armTwoDown(int speed, int angle)
 void pickUp()
 {
 	armOneDown(20, 20);
-	move(20, 20, -10 );
-	move(20, 30, -10);
+	move(20, 20, -10, 0);
+	move(20, 30, -10, 0);
 }
+
 void motorsOff()
 {
 	motor[motorA] = 0;
@@ -138,7 +204,16 @@ void motorsOff()
 
 task main()
 {
-	calibrate();
+	//wait1Msec(500);
+	//if (nNxtButtonPressed == -1)
+	//calibrate();
+	//rotateLeft(30,90);
 
+	//rotateRight(30,90);
+	//armOneUp(20,90);
 
+	//armOneDown(20,90);
+	//armTwoUp(40,90);
+	//armTwoDown(40,90);
+	move(20,0,0,-90);
 }
